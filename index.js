d7a1a9c4e02ea39478b210e65df04cf65f0d6045
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-08 17:21:51 -0600

    Arrow Functions with Parameters

--- index.js
+++ index.js
@@ -37,3 +37,9 @@ const PI = freezeObj();
 const magic = () => {
   return new Date();
 };
+//Arrow Functions with Parameters
+const myConcat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
