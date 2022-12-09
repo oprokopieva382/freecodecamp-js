574a51be1acebc2f9f688eddbc4d9f84d774bfe5
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-08 19:28:00 -0600

    Rest Parameter to Reassign Array Elements

--- index.js
+++ index.js
@@ -82,3 +82,10 @@ let a = 8,
   b = 6;
 [a, b] = [b, a];
 console.log(a, b);
+//Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+function removeFirstTwo(list) {
+  const [, , ...shorterList] = list; // Change this line
+  return shorterList;
+}
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
