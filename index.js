b0538d46191b5bac2aab48c6047952cf17a0fdd8
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-08 19:00:55 -0600

    Spread Operator to Evaluate Arrays In-Place

--- index.js
+++ index.js
@@ -49,3 +49,8 @@ const increment = (number = 1, value = 1) => number + value;
 const sum = (...args) => {
   return args.reduce((a, b) => a + b, 0);
 };
+//Spread Operator to Evaluate Arrays In-Place
+const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+let arr2;
+arr2 = [...arr1];
+console.log(arr2);
