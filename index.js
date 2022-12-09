ee58cc17f9279719343878a987a363efedc9979a
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-08 19:12:34 -0600

    Assign Variables from Nested Objects

--- index.js
+++ index.js
@@ -62,9 +62,18 @@ console.log(arr2);
 // };
 // const { today, tomorrow } = HIGH_TEMPERATURES;
 //Destructuring Assignment to Assign Variables from Objects
-const HIGH_TEMPERATURES = {
-  yesterday: 75,
-  today: 77,
-  tomorrow: 80,
+// const HIGH_TEMPERATURES = {
+//   yesterday: 75,
+//   today: 77,
+//   tomorrow: 80,
+// };
+// const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+//Destructuring Assignment to Assign Variables from Nested Objects
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 },
 };
-const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+const {
+  today: { low: lowToday, high: highToday },
+} = LOCAL_FORECAST;
