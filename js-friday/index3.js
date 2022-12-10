f9bd871412defb34ee47ea8da370282de71ebbb9
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-09 22:00:54 -0600

    Match Single Characters Not Specified

--- js-friday/index3.js
+++ js-friday/index3.js
@@ -8,31 +8,35 @@
 // let result2 = petRegex.test(petString);
 //
 //Ignore Case While Matching
-let myString = "freeCodeCamp";
-let fccRegex = /freeCodeCamp/i;
-let result = fccRegex.test(myString);
-//Extract Matches
-let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /coding/;
-let result2 = extractStr.match(codingRegex);
-//Find More Than the First Match
-let twinkleStar = "Twinkle, twinkle, little star";
-let starRegex = /Twinkle/gi;
-let result3 = twinkleStar.match(starRegex);
-//Match Anything with Wildcard Period
-let exampleStr = "Let's have fun with regular expressions!";
-let unRegex = /.un/;
-let result4 = unRegex.test(exampleStr);
-//Match Single Character with Multiple Possibilities
-let quoteSample =
-  "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[aeiou]/gi;
-let result5 = quoteSample.match(vowelRegex);
-//Match Letters of the Alphabet
-let quoteSample1 = "The quick brown fox jumps over the lazy dog.";
-let alphabetRegex = /[a-z]/gi;
-let result6 = quoteSample.match(alphabetRegex);
-//Match Numbers and Letters of the Alphabet
-let quoteSample2 = "Blueberry 3.141592653s are delicious.";
-let myRegex = /[h-s2-6]/gi;
-let result7 = quoteSample.match(myRegex);
+// let myString = "freeCodeCamp";
+// let fccRegex = /freeCodeCamp/i;
+// let result = fccRegex.test(myString);
+// //Extract Matches
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/;
+// let result2 = extractStr.match(codingRegex);
+// //Find More Than the First Match
+// let twinkleStar = "Twinkle, twinkle, little star";
+// let starRegex = /Twinkle/gi;
+// let result3 = twinkleStar.match(starRegex);
+// //Match Anything with Wildcard Period
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /.un/;
+// let result4 = unRegex.test(exampleStr);
+// //Match Single Character with Multiple Possibilities
+// let quoteSample =
+//   "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/gi;
+// let result5 = quoteSample.match(vowelRegex);
+// //Match Letters of the Alphabet
+// let quoteSample1 = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/gi;
+// let result6 = quoteSample.match(alphabetRegex);
+// //Match Numbers and Letters of the Alphabet
+// let quoteSample2 = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/gi;
+// let result7 = quoteSample.match(myRegex);
+//Match Single Characters Not Specified
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou^0-9]/gi;
+let result = quoteSample.match(myRegex);
