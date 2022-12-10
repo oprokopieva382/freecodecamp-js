b6b22bc9f9a6352c1b717a87300862fe623e48bd
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-09 20:23:19 -0600

    Literal String with Different Possibilities

--- js-friday/index3.js
+++ js-friday/index3.js
@@ -2,3 +2,7 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+//Match Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let result2 = petRegex.test(petString);
