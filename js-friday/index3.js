3534e148da917423c05a588a69ec921972b120fe
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-09 21:51:25 -0600

    Match Letters of the Alphabet

--- js-friday/index3.js
+++ js-friday/index3.js
@@ -28,3 +28,7 @@ let quoteSample =
   "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // Change this line
 let result5 = quoteSample.match(vowelRegex); // Change this line
+//Match Letters of the Alphabet
+let quoteSample1 = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result6 = quoteSample.match(alphabetRegex); // Change this line
