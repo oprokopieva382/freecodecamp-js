df4a14f505f40f693179b21c80478df41d21d5a1
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-09 20:09:13 -0600

    Handle a Rejected Promise with catch

--- js-friday/index.js
+++ js-friday/index.js
@@ -45,3 +45,6 @@ const makeServerRequest = new Promise((resolve, reject) => {
 makeServerRequest.then((result) => {
   console.log(result);
 });
+makeServerRequest.catch((error) => {
+  console.log(error);
+});
