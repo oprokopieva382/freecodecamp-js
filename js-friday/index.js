7815f08b7992daebf783d1833ae8b4c3f2f5b6cb
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-09 19:47:13 -0600

    export to Share a Code Block

--- js-friday/index.js
+++ js-friday/index.js
@@ -18,3 +18,13 @@ const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
+//export to Share a Code Block
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+};
+
+const lowercaseString = (string) => {
+  return string.toLowerCase();
+};
+
+export { uppercaseString, lowercaseString };
